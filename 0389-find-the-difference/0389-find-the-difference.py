5ed93ba1c83d6bef6a0823971a18b35d464479f8
Lokesh51862 <2303a51862@sru.edu.in>
2026-07-22 00:01:55 +0530

    Time: 2 ms (56.63%), Space: 19.4 MB (22.81%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        d={}
+        for i in s:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for i in t:
+            if i not in d:
+                return i
+            else:
+                d[i]-=1
+            if d[i]<0:
+                return i
+            
+        
